0a012b3057185f08b2322df398f79712db44aeef
K1C8 <cjak016@gmail.com>
2026-02-21 15:07:12 -0800

    Put in placeholder Enemy Manager

---
+++ Source/ProjectChimera/Variant_Combat/AI/CombatEnemyManager.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Variant_Combat/AI/CombatEnemyManager.h"
+#include "AbilitySystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "AbilitySystemGlobals.h"
+#include "RPGAttributeSet.h"
+#include "AttributeSet.h"
+#include "GameFramework/Character.h"
+//#include <mutex>
+
+// Sets default values
+ACombatEnemyManager::ACombatEnemyManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACombatEnemyManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Initial Setup
+	EnemyToSpawnCount = 3;
+	EliteEnemyToSpawnCount = 0;
+
+	// Binding delegate function for attribute listening
+	IAbilitySystemInterface* GasInterfacePtr = Cast<IAbilitySystemInterface>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if (GasInterfacePtr)
+	{
+		AbilitySystemComponent = GasInterfacePtr->GetAbilitySystemComponent();
+		if (IsValid(AbilitySystemComponent))
+		{
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(URPGAttributeSet::GetCurrentLevelAttribute()).AddUObject(this, &ACombatEnemyManager::OnPlayerLevelUp);
+		}
+	}
+		
+}
+
+// Called every frame
+void ACombatEnemyManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACombatEnemyManager::OnEnemySpawned()
+{
+	//std::lock_guard<std::mutex> Lock(Mtx);
+	EnemyToSpawnCount--;
+
+}
+
+void ACombatEnemyManager::OnEliteEnemySpawned()
+{
+	//std::lock_guard<std::mutex> Lock(Mtx);
+	EliteEnemyToSpawnCount--;
+}
+
+void ACombatEnemyManager::OnPlayerLevelUp(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Display, TEXT("Player level up. New level data %d"), (int32)Data.NewValue);
+}
+
---
+++ Source/ProjectChimera/Variant_Combat/AI/CombatEnemyManager.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Public/RPGAttributeSet.h"
+#include "AbilitySystemInterface.h"
+#include <AbilitySystemComponent.h>
+//#include <mutex>
+#include "CombatEnemyManager.generated.h"
+
+UCLASS()
+class PROJECTCHIMERA_API ACombatEnemyManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACombatEnemyManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Registers for enemy spawners to decrease enemy to spawn counters.
+	void OnEnemySpawned();
+	void OnEliteEnemySpawned();
+
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	/** List of actors to activate after the last enemy dies */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enemy Spawner")
+	TArray<AActor*> EnemySpawner;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Enemy Spawner")
+	TArray<AActor*> EliteEnemySpawner;
+
+	int32 EnemyToSpawnCount = 0;
+
+	int32 EliteEnemyToSpawnCount = 0;
+
+	//std::mutex Mtx;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called when player level up.
+	void OnPlayerLevelUp(const FOnAttributeChangeData& Data);
+
+};
