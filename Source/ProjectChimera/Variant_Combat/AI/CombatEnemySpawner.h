37a535e94bb2caaa1af929140d930d7d449f4d29
K1C8 <cjak016@gmail.com>
2026-02-21 12:42:37 -0800

    Changed combat level.

--- Source/ProjectChimera/Variant_Combat/AI/CombatEnemySpawner.h
+++ Source/ProjectChimera/Variant_Combat/AI/CombatEnemySpawner.h
@@ -43,7 +43,7 @@ protected:
 	float InitialSpawnDelay = 5.0f;
 
 	/** Number of enemies to spawn */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enemy Spawner", meta = (ClampMin = 0, ClampMax = 100))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Enemy Spawner", meta = (ClampMin = 0, ClampMax = 100))
 	int32 SpawnCount = 1;
 
 	/** Time to wait before spawning the next enemy after the current one dies */
